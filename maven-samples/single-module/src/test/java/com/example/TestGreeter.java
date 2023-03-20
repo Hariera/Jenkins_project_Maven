3986e527d8692cc0fff62b0b855d1da54bd81593
hariera8 <hariera8@gmail.com>
2023-03-20 08:39:34 +0530

    Added files

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -43,7 +43,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeGreetingMessage1() {
-    String someone = "Hello , This is example for POLLSCM and Github webhook added DDED abd dded";
+    String someone = "Hello , This is example for POLLSCM";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
     
