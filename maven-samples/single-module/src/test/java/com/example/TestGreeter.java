30247eef49f4c8fc845b5a65a2d4edbbb7eabca6
hariera8 <hariera8@gmail.com>
2023-04-23 08:55:35 +0530

    Added all

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -43,7 +43,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeGreetingMessage1() {
-    String someone = "Hello , This is example for POLLSCM..";
+    String someone = "Hello , This is example for jenkins";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
     
