63e9a48ede9b34ff060d52529e268fc2af1cab16
hariera8 <hariera8@gmail.com>
2023-07-03 06:44:35 +0530

    added all files into groovy

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -43,7 +43,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeGreetingMessage1() {
-    String someone = "Hello , This is example for  DSL";
+    String someone = "Hello , This is example for  DSL for groovy script";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
     
