7bc81a173f0c6b1f04fd583831d910e2e7d76f40
hariera8 <hariera8@gmail.com>
2023-07-10 07:19:20 +0530

    added allfiles

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -43,7 +43,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeGreetingMessage1() {
-    String someone = "Hello , This is example for  DSL for groovy script";
+    String someone = "Hello , This is example for  GITwebhook and poolscm";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
     
