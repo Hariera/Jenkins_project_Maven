5b9c4a469ff11b2404c08bdca94b160e85df2305
hariera8@gmail.com <hariera8@gmail.com>
2024-04-14 23:03:43 +0530

    Added all pollscm

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -43,7 +43,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeGreetingMessage1() {
-    String someone = "Hello , This is example for  GITwebhook and poolscm";
+    String someone = "Hello , This is example for  Pollscm";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
     
