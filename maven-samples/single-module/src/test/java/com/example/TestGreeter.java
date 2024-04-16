1c5ce0c811af1ae88b01b54d1aac6d7c9501250f
hariera8@gmail.com <hariera8@gmail.com>
2024-04-16 05:45:25 +0530

    Added all status of artifact

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -43,7 +43,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeGreetingMessage1() {
-    String someone = "Hello , This is example for webhook triggeer and Junit";
+    String someone = "Hello , This is example for webhook triggeer and Junit and artifact";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
     
