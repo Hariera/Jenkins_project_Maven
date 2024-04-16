c0bd2e2cecfe8d20ce4006c78153c528c6704fca
hariera8@gmail.com <hariera8@gmail.com>
2024-04-16 05:46:13 +0530

    added Jar fies

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -43,7 +43,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeGreetingMessage1() {
-    String someone = "Hello , This is example for webhook triggeer and Junit and artifact";
+    String someone = "Hello , This is example for webhook triggeer and Junit and artifact as Jar file";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
     
