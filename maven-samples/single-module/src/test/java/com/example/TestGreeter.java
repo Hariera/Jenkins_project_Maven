a27bd267be26845d574ab28c82a1cb9ec0c784f5
Hariera <98234184+Hariera@users.noreply.github.com>
2024-04-28 19:28:56 +0530

    Update TestGreeter.java

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -43,7 +43,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeGreetingMessage1() {
-    String someone = "Hello , This is sample project for POLLSCM for every 2 min;
+    String someone = "Hello , This is sample project for POLLSCM for every 2 min"
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
     
