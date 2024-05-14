5ee0bcbb0e051b034960304b9e07753ff865feab
hariera8 <hariera8@gmail.com>
2024-05-14 18:29:07 +0530

    added for 2 min PULL SCM

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -43,7 +43,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeGreetingMessage1() {
-    String someone = "Hello , This is sample project for PULL SCM";
+    String someone = "Hello , This is sample project for PULL SCM for 2 min";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
     
