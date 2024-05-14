3351a5dc7d8a97b269c6f2c56f007da64201f40d
hariera8 <hariera8@gmail.com>
2024-05-14 17:08:51 +0530

    ADDED all into webhook

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -43,7 +43,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeGreetingMessage1() {
-    String someone = "Hello , This is sample project for webhook pulling SCM";
+    String someone = "Hello , This is sample project for webhook pullingSCM";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
     
