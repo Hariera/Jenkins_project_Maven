5c96a1eb91b44827705aa36228d32a77c3618ffb
hariera8 <hariera8@gmail.com>
2024-05-19 13:39:07 +0530

    added all SCM

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -43,7 +43,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeGreetingMessage1() {
-    String someone = "Hello , This is sample project for webhook for pullscm ";
+    String someone = "Hello , This is sample project for SCM to Jenkins";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
     
