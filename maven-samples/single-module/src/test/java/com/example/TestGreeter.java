63859eb30e22e1919ee66051e161613dd8ccad4d
hariera8 <hariera8@gmail.com>
2024-05-19 13:35:48 +0530

    added all POLLSCM

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -43,7 +43,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeGreetingMessage1() {
-    String someone = "Hello , This is sample project for webhook for jenkins will listen to webhook and github will givw info to jenkins ";
+    String someone = "Hello , This is sample project for webhook for pullscm ";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
     
