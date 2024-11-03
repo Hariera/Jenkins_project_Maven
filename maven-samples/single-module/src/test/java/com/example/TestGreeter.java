dba06b88e7a91d620a5614b202d4cf84c9816263
hariera8 <hariera8@gmail.com>
2024-11-03 13:13:26 +0530

    Added all onto LR

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -43,7 +43,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeGreetingMessage1() {
-    String someone = "Hello , This is sample project for webhook for jenkins trigger for maven job";
+    String someone = "Hello , This is sample project for webhook trigger";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
     
